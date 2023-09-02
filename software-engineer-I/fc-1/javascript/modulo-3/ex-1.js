ec8080f172f2cb7ff7f9227fc4e928f0b72f8632
Luana Luz <luanarahal@gmail.com>
2023-09-02 03:36:39 -0300

    fix: removed folders

--- software-engineer-I/fc-1/javascript/modulo-3/ex-1.js
+++
@@ -1,9 +0,0 @@
-const base = 10;
-const height = 20;
-
-const calculateTriangleArea = (base, height) => {
-    const area = (base * height) / 2;
-    return area;
-}
-
-console.log(calculateTriangleArea(base, height));
--- software-engineer-I/fc-1/javascript/modulo-3/ex-8.js
+++
@@ -1,9 +0,0 @@
-const salary = 10000;
-const salaryIncrease = 30;
-
-const checkSalaryIncrease = (salary, salaryIncrease) => {
-    const  salaryIncreased = salary * (salaryIncrease / 100);
-    return salary + salaryIncreased;
-}
-
-console.log(checkSalaryIncrease(salary, salaryIncrease));
