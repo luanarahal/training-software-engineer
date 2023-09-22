951f422dc00c624f253612bf4f97e68099cb5ce4
Luana Luz <luanarahal@gmail.com>
2023-09-22 14:00:18 -0300

    feat: grade calculator

--- associate-software-engineer/fc-1/javascript/modulo-1/ex-1.js
+++ associate-software-engineer/fc-1/javascript/modulo-1/ex-1.js
@@ -1,11 +1,11 @@
-salesPedro = 100
-salesMaria = 150
-salesCarlos = 200
+salesPedro = 100;
+salesMaria = 150;
+salesCarlos = 200;
 
 if(salesPedro > salesMaria) {
-    console.log("Pedro vendeu mais!")
+    console.log("Pedro vendeu mais!");
 } else if (salesMaria > salesCarlos) {
-    console.log("Maria vendeu mais!")
+    console.log("Maria vendeu mais!");
 } else {
-    console.log("Carlos vendeu mais!")
+    console.log("Carlos vendeu mais!");
 }
---
+++ associate-software-engineer/fc-1/javascript/modulo-1/ex-3.js
@@ -0,0 +1,34 @@
+grade = 10;
+
+switch(grade) {
+    case 1:
+        console.log("Você está reprovado!");
+        break;
+    case 2:
+        console.log("Você está reprovado!");
+        break;
+    case 3:
+        console.log("Você está reprovado!");
+        break;
+    case 4:
+        console.log("Você está reprovado!");
+        break;
+    case 5:
+        console.log("Você está reprovado!");
+        break;
+    case 6:
+        console.log("Você está de recuperação!");
+        break;        
+    case 7:
+        console.log("Você está aprovado!");
+        break;
+    case 8:
+        console.log("Você está aprovado!");
+        break;
+    case 9:
+        console.log("Você está aprovado!");
+        break;
+    case 10:
+        console.log("Você está aprovado!");
+        break;
+}
