b238943d4ec8b44ad30cb52200a5fc4d667925e9
Luana Luz <luanarahal@gmail.com>
2023-09-22 12:24:48 -0300

    feat: best seller

---
+++ associate-software-engineer/fc-1/javascript/modulo-1/ex-1.js
@@ -0,0 +1,11 @@
+salesPedro = 100
+salesMaria = 150
+salesCarlos = 200
+
+if(salesPedro > salesMaria) {
+    console.log("Pedro vendeu mais!")
+} else if (salesMaria > salesCarlos) {
+    console.log("Maria vendeu mais!")
+} else {
+    console.log("Carlos vendeu mais!")
+}
