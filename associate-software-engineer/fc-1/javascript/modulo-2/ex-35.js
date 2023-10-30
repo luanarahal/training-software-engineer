97c2653d6ca6d1360b932e97a8a0f1ea6af5a106
Luana Luz <luanarahal@gmail.com>
2023-10-30 14:49:37 -0300

    fix: ex-25

--- associate-software-engineer/fc-1/javascript/modulo-2/ex-35.js
+++ associate-software-engineer/fc-1/javascript/modulo-2/ex-35.js
@@ -1,18 +1,13 @@
 import { students } from "./mock.js";
 
 const listStudentsAlphabetically = students
-    .map(student => `Nome: ${student.nome} - Idade: ${student.idade}`)
     .sort((student1, student2) => {
-        if(student1.nome < student2.nome) {
-            return student1;
-        } else if(student1.nome === student2.nome){
-            if (student1.idade < student2.idade) {
-                return student1;
-            }
-            else {
-                return student2;
-            }
+        if(student1.nome[0] === student2.nome[0]) {
+            return student1.idade.toString().localeCompare(student2.idade.toString())
+        } else {
+            return student1.nome.localeCompare(student2.nome)
         }
-    });
+    })
+    .map(student => `Nome: ${student.nome} - Idade: ${student.idade}`);
 
 console.log(listStudentsAlphabetically);
