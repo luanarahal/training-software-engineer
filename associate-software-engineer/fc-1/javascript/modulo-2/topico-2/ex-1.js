0e0245cb09f564586c38bbf76ee1924d5d7361c9
Luana Luz <luanarahal@gmail.com>
2023-11-15 22:41:00 -0300

    feat: ex-2

---
+++ associate-software-engineer/fc-1/javascript/modulo-2/topico-2/ex-1.js
@@ -0,0 +1,5 @@
+const book = {
+    title: "Diário de Anne Frank",
+    author: "Anne Frank",
+    year: 1947
+}
---
+++ associate-software-engineer/fc-1/javascript/modulo-2/topico-2/ex-2.js
@@ -0,0 +1,8 @@
+const book = {
+    title: "Diário de Anne Frank",
+    author: "Anne Frank",
+    year: 1947
+}
+
+book.publishingCompany = 'Contact';
+console.log(book);
