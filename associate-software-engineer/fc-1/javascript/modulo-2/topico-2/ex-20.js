e1d833fc4574d0b4af70d955917d0f3360876a7d
Luana Luz <luanarahal@gmail.com>
2023-11-21 23:52:46 -0300

    fix: Antonio's comments

--- associate-software-engineer/fc-1/javascript/modulo-2/topico-2/ex-20.js
+++ associate-software-engineer/fc-1/javascript/modulo-2/topico-2/ex-20.js
@@ -1,12 +1,14 @@
 const keys = ['chave1', 'chave2', 'chave3'];
 const values = ['valor1', 'valor2', 'valor3'];
 
-function arraysToMap(index) {
+const arraysToMap = (keys, values) => {
     const newMap = new Map();
 
     if (keys.length === values.length) {
-        for (index in keys) {
-            newMap.set(keys[index], values[index]);
+        let index = 0;
+        for (const key of keys) {
+            newMap.set(key, values[index]);
+            index++;
         }
     } else {
         console.log('Os arrays tem comprimentos diferentes, então não é possível relacioná-los');
