24ae297892f853d798007a4acd41ccdfff37df0a
Luana Luz <luanarahal@gmail.com>
2023-12-11 23:49:20 -0300

    ex-3

---
+++ associate-software-engineer/fc-1/javascript/modulo-3/topico-1/ex-3.js
@@ -0,0 +1,8 @@
+function doubleValues(number) {
+    return number * 2;
+}
+
+const doubleValuesArrowFunction = (number) => number * 2;
+
+console.log(doubleValues(2));
+console.log(doubleValuesArrowFunction(3));
