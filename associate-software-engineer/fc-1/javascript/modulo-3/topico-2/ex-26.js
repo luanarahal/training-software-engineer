79f33d4bd621336c233af2aea19f22637cb6404f
Luana Luz <luanarahal@gmail.com>
2023-12-15 13:58:38 -0300

    ex-26

---
+++ associate-software-engineer/fc-1/javascript/modulo-3/topico-2/ex-26.js
@@ -0,0 +1,6 @@
+import { students } from "./mock.js";
+
+const sortStudentsByBirthDate = students
+    .sort((student1, student2) => new Date(student2.dataNascimento) - new Date(student1.dataNascimento));
+
+console.log(sortStudentsByBirthDate);
