2a91faa0e1c5d0b521990581eee52ab3d94301ba
Luana Luz <luanarahal@gmail.com>
2023-12-14 21:08:22 -0300

    fix: Antonio's comments

--- associate-software-engineer/fc-1/javascript/modulo-3/topico-2/ex-8.js
+++ associate-software-engineer/fc-1/javascript/modulo-3/topico-2/ex-8.js
@@ -1,9 +1,8 @@
 function classifyProduct(amount) {
     if (amount < 10) {
         return `O produto está acabando!`;
-    } else if (amount >= 10) {
-        return `Quantidade OK`;
-    }
+    } 
+    return `Quantidade OK`;
 }
 
-console.log(classifyProduct(5));
+console.log(classifyProduct(55));
