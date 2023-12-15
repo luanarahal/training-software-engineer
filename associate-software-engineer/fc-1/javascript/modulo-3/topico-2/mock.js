6d4fbc0cd0bd64d10fdd5dafa8d8a1afe71771cd
Luana Luz <luanarahal@gmail.com>
2023-12-14 21:52:09 -0300

    ex-11

---
+++ associate-software-engineer/fc-1/javascript/modulo-3/topico-2/mock.js
@@ -0,0 +1,22 @@
+export const students = [
+    {
+        nome: "Luana",
+        idade: 17,
+        dataNascimento: "09-12-1996"
+    },
+    {
+        nome: "Adriano",
+        idade: 27,
+        dataNascimento: "30-05-1996"
+    },
+    {
+        nome: "Natalia",
+        idade: 45,
+        dataNascimento: "09-03-1978"
+    },
+    {
+        nome: "Marco",
+        idade: 46,
+        dataNascimento: "13-05-1977"
+    }
+];
