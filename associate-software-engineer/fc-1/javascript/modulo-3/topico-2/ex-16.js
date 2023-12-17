a91a49214f6aea1b575359d11a5daa370c781bfe
Luana Luz <luanarahal@gmail.com>
2023-12-17 12:38:57 -0300

    fix: Antonio's comments

--- associate-software-engineer/fc-1/javascript/modulo-3/topico-2/ex-16.js
+++ associate-software-engineer/fc-1/javascript/modulo-3/topico-2/ex-16.js
@@ -1,6 +1,6 @@
 import { students } from "./mock.js";
 
-function verifyRange(age) {
+function getRange(age) {
     let ageRange;
     if (age > 0 && age <= 12) {
         ageRange = '0-12';
@@ -18,7 +18,7 @@ function orderStudentsByAgeGroup(students) {
     return students
         .reduce((total, student) => {
             const age = student.idade;
-            let ageRange = verifyRange(age);
+            let ageRange = getRange(age);
             
             if (!total[ageRange]) {
                 total[ageRange] = [];
