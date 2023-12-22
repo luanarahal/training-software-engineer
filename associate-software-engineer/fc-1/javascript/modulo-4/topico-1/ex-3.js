028588280ffec7406c39e2249d1876ce3e7effa7
Luana Luz <luanarahal@gmail.com>
2023-12-22 12:01:51 -0300

    ex-8 and fix Antonio's comments

--- associate-software-engineer/fc-1/javascript/modulo-4/topico-1/ex-3.js
+++ associate-software-engineer/fc-1/javascript/modulo-4/topico-1/ex-3.js
@@ -1,13 +1,13 @@
 const validateEmail = (email) => {
     const emailPattern =  /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
-    if (emailPattern.test(email)) {
-        return "E-mail cadastrado com sucesso!";
+    if (!emailPattern.test(email)) {
+        throw new Error("E-mail inserido é inválido!")
     }
-    return error; 
+    return "E-mail cadastrado com sucesso!"
 }
 
 try {
     console.log(validateEmail('luanarahal@gmail.com'));
 } catch (error) {
-    console.error("Formato de e-mail inválido!")
+    console.error(error.message);
 }
--- associate-software-engineer/fc-1/javascript/modulo-4/topico-1/ex-6.js
+++ associate-software-engineer/fc-1/javascript/modulo-4/topico-1/ex-6.js
@@ -2,18 +2,20 @@ const closeConection = () => {
     return console.log("---Conexão fechada.");
 }
 
+const openConection = () => {
+    return console.log("Conexão aberta---")
+}
+
 const calculateYearByAgeAndBirthYear = (age, birthYear) => {
-    if(age < 0) {
-        throw new Error("Idade não pode ser negativa!")
-    }
-    if(!age) {
+    if(!age || age < 0) {
         throw new Error("Formato de idade inválido!");
     }
     return age + birthYear;
 }
 
 try {
-    console.log(`O ano é: ${calculateYearByAgeAndBirthYear(10, 1996)}!`);
+    openConection();
+    console.log(`O ano é: ${calculateYearByAgeAndBirthYear(-10, 1996)}!`);
 } catch (error) {
     console.error("Erro capturado:", error.message);
 } finally {
