4d9997d68edc0ca022fb499716d3677fe660d618
Luana Luz <luanarahal@gmail.com>
2024-01-16 00:45:55 -0300

    ex-4

---
+++ associate-software-engineer/fc-1/javascript/modulo-5/topico-3/ex-4.js
@@ -0,0 +1,25 @@
+import { generateRandomNumber } from "./utils.js";
+
+const verifyNumber = () => {
+    const randomNumber = generateRandomNumber(0, 1, 2);
+    console.log(randomNumber);
+    return new Promise((resolve, reject) => {
+        if (randomNumber >= 0.5) {
+            resolve("O número é maior ou igual a 0.5!");
+        } else {
+            reject("O número é menor que 0.5!");
+        }
+    });
+}
+
+const main = async () => {
+    try {
+        const data = await verifyNumber();
+        console.log(data);
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+main();
