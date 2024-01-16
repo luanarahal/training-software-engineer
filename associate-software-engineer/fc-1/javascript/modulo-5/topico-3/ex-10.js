2bad36efab166d7de4252f121414301ab3b70854
Luana Luz <luanarahal@gmail.com>
2024-01-16 01:53:06 -0300

    fix: Antonio's comments

--- associate-software-engineer/fc-1/javascript/modulo-5/topico-3/ex-10.js
+++ associate-software-engineer/fc-1/javascript/modulo-5/topico-3/ex-10.js
@@ -20,7 +20,6 @@ const main = async () => {
     } catch (err) {
         console.error(err);
     }
-
 }
 
 main();
--- associate-software-engineer/fc-1/javascript/modulo-5/topico-3/ex-4.js
+++ associate-software-engineer/fc-1/javascript/modulo-5/topico-3/ex-4.js
@@ -2,7 +2,6 @@ import { generateRandomNumber } from "../utils.js";
 
 const verifyNumber = () => {
     const randomNumber = generateRandomNumber(0, 1, 2);
-    console.log(randomNumber);
     return new Promise((resolve, reject) => {
         if (randomNumber >= 0.5) {
             resolve("O número é maior ou igual a 0.5!");
@@ -16,8 +15,7 @@ const main = async () => {
     try {
         const data = await verifyNumber();
         console.log(data);
-    }
-    catch (error) {
+    } catch (error) {
         console.error(error);
     }
 }
