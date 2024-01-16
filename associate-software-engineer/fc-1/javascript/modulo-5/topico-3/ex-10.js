c40a09d70192ed47cbf3efb1ac03d33a7d34d7e6
Luana Luz <luanarahal@gmail.com>
2024-01-16 01:12:23 -0300

    ex-10

---
+++ associate-software-engineer/fc-1/javascript/modulo-5/topico-3/ex-10.js
@@ -0,0 +1,26 @@
+import { generateRandomNumber } from "../utils.js";
+
+const discoverNumber = (number) => {
+    const randomNumber = generateRandomNumber(0, 10);
+    console.log(`Número digitado: ${number}`);
+    console.log(`Número gerado: ${randomNumber}`);
+    return new Promise((resolve, reject) => {
+        if (number === randomNumber) {
+            resolve("Acertou!");
+        } else {
+            reject("Errou!");
+        }
+    });
+}
+
+const main = async () => {
+    try {
+        const data = await discoverNumber(5);
+        console.log(data);
+    } catch (err) {
+        console.error(err);
+    }
+
+}
+
+main();
