5c33e8261da6f44eda47cecaba84e71085ea89e3
Luana Luz <luanarahal@gmail.com>
2024-02-14 20:39:58 -0300

    fix: Antonio's comments

--- associate-software-engineer/fc-1/javascript/modulo-7/topico-1/index.js
+++ associate-software-engineer/fc-1/javascript/modulo-7/topico-1/index.js
@@ -4,13 +4,13 @@ title.textContent = "A Luana não é só linda, como ela é maravilhosa!!!";
 
 //Exercício 2
 const paragraphs = document.getElementsByTagName("p");
-for (let paragraph of paragraphs) {
+for (const paragraph of paragraphs) {
     paragraph.style.color = "#D35A93";
 };
 
 //Exercício 3
-const highlights = document.getElementsByClassName("emphasis")[0];
-highlights.style.backgroundColor = "pink";
+const highlight = document.getElementsByClassName("emphasis")[0];
+highlight.style.backgroundColor = "pink";
 
 //Exercício 4
 const items = document.querySelector(".item");
@@ -18,7 +18,7 @@ items.textContent = "Pode discordar, mas é a realidade!";
 
 //Exercício 5
 const links = document.querySelectorAll("a.link");
-for (let link of links) {
+for (const link of links) {
     link.setAttribute("target", "_blank");
 }
 
@@ -28,19 +28,18 @@ button.textContent = "Não pressione aqui!"
 
 //Exercício 7
 const items2 = document.querySelectorAll(".item");
-for (let item of items2) {
+for (const item of items2) {
     item.style.backgroundColor = "#A0144F";
 }
 
 //Exercício 8
 const highlights2 = document.getElementsByClassName("emphasis");
-for (let emphase of highlights2) {
+for (const emphase of highlights2) {
     emphase.innerText = "Texto alterado!";
 }
-console.log(empha);
 
 //Exercício 9
 const periods = document.getElementsByTagName("span");
-for (let period of periods) {
+for (const period of periods) {
     period.style.fontStyle = 'italic';
 }
